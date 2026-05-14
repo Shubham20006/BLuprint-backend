5d897a6d31f0015e8b2f4492de57efbf620c491d
BL-ShubhamChaudhari <shubham.chaudhari@bridgelabz.com>
2026-05-14 16:48:05 +0530

    Add: backend for bluprint

--- server.js
+++ server.js
@@ -7,13 +7,17 @@ const authRoutes = require('./routes/auth');
 const authMiddleware = require('./middleware/auth');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get('/', (req, res) => {
+  res.send('BLuprintAI API Server is running');
+});
+
 app.use('/auth', authRoutes);
 app.use('/api', authMiddleware, apiRoutes);
 
@@ -25,7 +29,7 @@ mongoose
   })
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
